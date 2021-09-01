f2c78acdbb9c3f8a844e1b8f7f11f7fb1fbf3695
Austin de Coup-Crank <austindcc@gmail.com>
2021-09-01 10:35:26 -0700

    refactor logging

--- find_orphans.py
+++ find_orphans.py
@@ -1,3 +1,4 @@
+import argparse
 from concurrent.futures import ThreadPoolExecutor
 import csv
 import os
@@ -13,25 +14,40 @@ import logging
 from datetime import datetime
 import yaml
 
-with open("./config.yml", "r") as fh:
-    config = yaml.safe_load(fh)
-
-log_level = getattr(logging, config['log_level'].upper(), None)
-if not isinstance(log_level, int):
-    raise ValueError(f'Invalid log level: {log_level}')
 
 logging.getLogger("paramiko.transport").disabled = True
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 root_log = logging.getLogger()
 log = logging.getLogger("find_orphans")
-log.setLevel(log_level)
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(name)s %(levelname)s: %(message)s")
 handler.setFormatter(formatter)
 root_log.addHandler(handler)
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", '-c', help="Path to YAML config file (defaults to ./config.yml)", type=str)
+args = parser.parse_args()
+
+if not args.config:
+    config_file = './config.yml'
+else:
+    config_file = args.config
+
+with open(config_file, "r") as fh:
+    config = yaml.safe_load(fh)
+log.debug(f'Opened config file {config_file}')
+
+log_level = getattr(logging, config['log_level'].upper(), None)
+if not isinstance(log_level, int):
+    raise ValueError(f'Invalid log level: {log_level}')
+else:
+    log.setLevel(log_level)
+    log.debug(f'Set log level: {config["log_level"]}')
+
 if not config['validate_certs']:
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    log.debug(f'Ignoring cert warnings for {config["npm_server"]}')
 
 # netmiko uses this environment variable for textfsm templates
 os.environ["NET_TEXTFSM"] = config["ntc_templates_path"]
@@ -39,8 +55,9 @@ os.environ["NET_TEXTFSM"] = config["ntc_templates_path"]
 npm_client = orionsdk.SwisClient(
     config["npm_server"], config["npm_username"], config["npm_password"]
 )
+log.debug(f'Connected to {config["npm_server"]} as {config["npm_username"]}')
 npm_results = npm_client.query(config["npm_query"])["results"]
-logging.info(f"Found {len(npm_results)} NPM results")
+log.info(f"Retrieved {len(npm_results)} NPM results")
 
 net_device_dict = {
     "device_type": "cisco_ios",
@@ -50,6 +67,7 @@ net_device_dict = {
 }
 if config["ssh_config_file"]:
     net_device_dict.update({"ssh_config_file": config["ssh_config_file"]})
+    log.debug(f'Using SSH config file {config["ssh_config_file"]}')
 
 
 def in_npm_results(nbr):
@@ -63,20 +81,26 @@ def in_npm_results(nbr):
     return in_results
 
 
-def i_care(hostname, nbr):
+def i_care(hostname=None, nbr=None):
     """ Whether or not I care about a given host or neighbor """
-    for expr in config["ignore_hosts"]:
-        if re.search(expr, hostname):
-            return False
-
-    for expr in config["ignore_neighbors"]:
-        if re.search(expr, nbr["destination_host"]):
-            return False
-
-    if "Router" in nbr["capabilities"] or "Switch" in nbr["capabilities"]:
+    if hostname:
+        for expr in config["ignore_hosts"]:
+            if re.search(expr, hostname):
+                log.debug(f"Host {hostname} matches expression {expr} in ignore_hosts, ignoring")
+                return False
         return True
-    else:
-        return False
+
+    if nbr:
+        for expr in config["ignore_neighbors"]:
+            if re.search(expr, nbr["destination_host"]):
+                log.debug(f"Neighbor {nbr['destination_host']} matches expression {expr} in ignore_neighbors, ignoring")
+                return False
+
+        if "Router" in nbr["capabilities"] or "Switch" in nbr["capabilities"]:
+            return True
+        else:
+            log.debug(f'Neighbor {nbr["destination_host"]} is neither router nor switch, ignoring')
+            return False
 
 
 def get_cdp_neighbors(device):
@@ -133,20 +157,13 @@ def main():
     # Build netmiko device connection dicts for all NPM hosts we care about
     net_devices = []
     for host in npm_results:
-        ignore = False
-        for expr in config["ignore_hosts"]:
-            if re.search(expr, host["hostname"]):
-                log.debug(
-                    f'Hostname {host["hostname"]} matches expression "{expr}" in ignore_hosts, ignoring'
-                )
-                ignore = True
-        if not ignore:
+        if i_care(hostname=host["hostname"]):
             my_device_dict = copy.copy(net_device_dict)
             my_device_dict["host"] = host["hostname"]
             net_devices.append(my_device_dict)
 
     cdp_results = {}
-    log.info(
+    log.debug(
         f'Gathering CDP neighbors from {len(net_devices)} devices with {config["max_threads"]} threads'
     )
     with ThreadPoolExecutor(max_workers=config["max_threads"]) as executor:
@@ -158,12 +175,14 @@ def main():
         for hostname, status in cdp_result.items():
             if status["success"]:
                 for nbr in status["neighbors"]:
-                    if i_care(hostname, nbr):
+                    if i_care(nbr=nbr):
                         nbr_hostname = format_neighbor(nbr["destination_host"])
                         nbr_port = nbr["local_port"]
                         nbr_ip = nbr["management_ip"]
                         nbr_platform = nbr["platform"]
-                        if not in_npm_results(nbr):
+                        if in_npm_results(nbr):
+                            log.debug(f'Neighbor {nbr_hostname} is known to NPM')
+                        else:
                             log.info(
                                 f"Found orphan: {nbr_hostname} ({nbr_ip}) on {hostname} port {nbr_port}"
                             )
